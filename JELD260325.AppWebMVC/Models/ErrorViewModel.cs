617b5e02a5a54f4ee39f4302902610a55d4022d2
Jazmin <ACER@LAPTOP-FRMPB979>
2025-03-26 22:10:25 -0600

    Agregar archivos de proyecto.

---
+++ JELD260325.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JELD260325.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
